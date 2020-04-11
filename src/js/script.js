2049172dd5bbc74fb5b19d8ccc0b409c8f262a9d
Taras <taras.polischuk@gmail.com>
2020-04-11 17:57:13 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/carousel/arrow_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-prev"><img src="../icons/carousel/arrow_right.svg"></button>'
+      });
+  });
