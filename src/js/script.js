59d45b5dce475f8f6494d0a7c0493bf773cda9d9
Taras <taras.polischuk@gmail.com>
2020-04-11 19:43:46 +0200

    change direction in src items

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,6 @@ $(document).ready(function(){
         speed: 1200,
         // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/arrow_left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/arrow_right.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/arrow_right.svg"></button>', 
       });
   });
