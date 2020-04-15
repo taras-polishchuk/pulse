bd2bf38e53f37449927ea947bdeaf14d96a0067f
Taras <taras.polischuk@gmail.com>
2020-04-15 20:34:02 +0200

    adapted the catalog and slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        // adaptiveHeight: true,
+        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/carousel/arrow_left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/carousel/arrow_right.svg"></button>', 
-      });
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+});
+
+
 
         $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
             $(this)
